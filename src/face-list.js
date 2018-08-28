7dcc59172b46cd69d7180e125cd10eaa7188a664
EdgarAllanzp <edgarallan@qq.com>
2018-08-28 13:45:48 +0800

    Add face list.

---
+++ src/face-list.js
@@ -0,0 +1,445 @@
+
+const qqfaceList = [
+  {
+      symbol: '[微笑]',
+      name: 'weixiao',
+      title: '微笑'
+  },
+  {
+      symbol: '[撇嘴]',
+      name: 'piezui',
+      title: '撇嘴'
+  },
+  {
+      symbol: '[色]',
+      name: 'se',
+      title: '色'
+  },
+  {
+      symbol: '[发呆]',
+      name: 'fadai',
+      title: '发呆'
+  },
+  {
+      symbol: '[得意]',
+      name: 'deyi',
+      title: '得意'
+  },
+  {
+      symbol: '[流泪]',
+      name: 'liulei',
+      title: '流泪'
+  },
+  {
+      symbol: '[害羞]',
+      name: 'haixiu',
+      title: '害羞'
+  },
+  {
+      symbol: '[闭嘴]',
+      name: 'bizui',
+      title: '闭嘴'
+  },
+  {
+      symbol: '[睡]',
+      name: 'shui',
+      title: '睡'
+  },
+  {
+      symbol: '[大哭]',
+      name: 'daku',
+      title: '大哭'
+  },
+  {
+      symbol: '[尴尬]',
+      name: 'ganga',
+      title: '尴尬'
+  },
+  {
+      symbol: '[发怒]',
+      name: 'fanu',
+      title: '发怒'
+  },
+  {
+      symbol: '[调皮]',
+      name: 'tiaopi',
+      title: '调皮'
+  },
+  {
+      symbol: '[呲牙]',
+      name: 'ciya',
+      title: '呲牙'
+  },
+  {
+      symbol: '[惊讶]',
+      name: 'jingya',
+      title: '惊讶'
+  },
+  {
+      symbol: '[难过]',
+      name: 'nanguo',
+      title: '难过'
+  },
+  {
+      symbol: '[冷汗]',
+      name: 'lenghan',
+      title: '冷汗'
+  },
+  {
+      symbol: '[抓狂]',
+      name: 'zhuakuang',
+      title: '抓狂'
+  },
+  {
+      symbol: '[吐]',
+      name: 'tu',
+      title: '吐'
+  },
+  {
+      symbol: '[偷笑]',
+      name: 'touxiao',
+      title: '偷笑'
+  },
+  {
+      symbol: '[愉快]',
+      name: 'keai',
+      title: '愉快'
+  },
+  {
+      symbol: '[白眼]',
+      name: 'baiyan',
+      title: '白眼'
+  },
+  {
+      symbol: '[傲慢]',
+      name: 'aoman',
+      title: '傲慢'
+  },
+  {
+      symbol: '[饥饿]',
+      name: 'jie',
+      title: '饥饿'
+  },
+  {
+      symbol: '[困]',
+      name: 'kun',
+      title: '困'
+  },
+  {
+      symbol: '[惊恐]',
+      name: 'jingkong',
+      title: '惊恐'
+  },
+  {
+      symbol: '[流汗]',
+      name: 'liuhan',
+      title: '流汗'
+  },
+  {
+      symbol: '[憨笑]',
+      name: 'hanxiao',
+      title: '憨笑'
+  },
+  {
+      symbol: '[悠闲]',
+      name: 'youxian',
+      title: '悠闲'
+  },
+  {
+      symbol: '[奋斗]',
+      name: 'fendou',
+      title: '奋斗'
+  },
+  {
+      symbol: '[咒骂]',
+      name: 'zhouma',
+      title: '咒骂'
+  },
+  {
+      symbol: '[疑问]',
+      name: 'yiwen',
+      title: '疑问'
+  },
+  {
+      symbol: '[嘘]',
+      name: 'xu',
+      title: '嘘'
+  },
+  {
+      symbol: '[晕]',
+      name: 'yun',
+      title: '晕'
+  },
+  {
+      symbol: '[衰]',
+      name: 'shuai',
+      title: '衰'
+  },
+  {
+      symbol: '[骷髅]',
+      name: 'kulou',
+      title: '骷髅'
+  },
+  {
+      symbol: '[敲打]',
+      name: 'qiaoda',
+      title: '敲打'
+  },
+  {
+      symbol: '[再见]',
+      name: 'zaijian',
+      title: '再见'
+  },
+  {
+      symbol: '[擦汗]',
+      name: 'cahan',
+      title: '擦汗'
+  },
+  {
+      symbol: '[抠鼻]',
+      name: 'koubi',
+      title: '抠鼻'
+  },
+  {
+      symbol: '[鼓掌]',
+      name: 'guzhang',
+      title: '鼓掌'
+  },
+  {
+      symbol: '[糗大了]',
+      name: 'qiudale',
+      title: '糗大了'
+  },
+  {
+      symbol: '[坏笑]',
+      name: 'huaixiao',
+      title: '坏笑'
+  },
+  {
+      symbol: '[左哼哼]',
+      name: 'zuohengheng',
+      title: '左哼哼'
+  },
+  {
+      symbol: '[右哼哼]',
+      name: 'youhengheng',
+      title: '右哼哼'
+  },
+  {
+      symbol: '[哈欠]',
+      name: 'haqian',
+      title: '哈欠'
+  },
+  {
+      symbol: '[鄙视]',
+      name: 'bishi',
+      title: '鄙视'
+  },
+  {
+      symbol: '[委屈]',
+      name: 'weiqu',
+      title: '委屈'
+  },
+  {
+      symbol: '[快哭了]',
+      name: 'kuaikule',
+      title: '快哭了'
+  },
+  {
+      symbol: '[阴险]',
+      name: 'yinxian',
+      title: '阴险'
+  },
+  {
+      symbol: '[亲亲]',
+      name: 'qinqin',
+      title: '亲亲'
+  },
+  {
+      symbol: '[吓]',
+      name: 'xia',
+      title: '吓'
+  },
+  {
+      symbol: '[可怜]',
+      name: 'kelian',
+      title: '可怜'
+  },
+  {
+      symbol: '[菜刀]',
+      name: 'caidao',
+      title: '菜刀'
+  },
+  {
+      symbol: '[西瓜]',
+      name: 'xigua',
+      title: '西瓜'
+  },
+  {
+      symbol: '[啤酒]',
+      name: 'pijiu',
+      title: '啤酒'
+  },
+  {
+      symbol: '[咖啡]',
+      name: 'kafei',
+      title: '咖啡'
+  },
+  {
+      symbol: '[猪头]',
+      name: 'zhutou',
+      title: '猪头'
+  },
+  {
+      symbol: '[玫瑰]',
+      name: 'meigui',
+      title: '玫瑰'
+  },
+  {
+      symbol: '[凋谢]',
+      name: 'diaoxie',
+      title: '凋谢'
+  },
+  {
+      symbol: '[嘴唇]',
+      name: 'zuichun',
+      title: '嘴唇'
+  },
+  {
+      symbol: '[爱心]',
+      name: 'aixin',
+      title: '爱心'
+  },
+  {
+      symbol: '[心碎]',
+      name: 'xinsui',
+      title: '心碎'
+  },
+  {
+      symbol: '[蛋糕]',
+      name: 'dangao',
+      title: '蛋糕'
+  },
+  {
+      symbol: '[炸弹]',
+      name: 'zhadan',
+      title: '炸弹'
+  },
+  {
+      symbol: '[便便]',
+      name: 'bianbian',
+      title: '便便'
+  },
+  {
+      symbol: '[月亮]',
+      name: 'yueliang',
+      title: '月亮'
+  },
+  {
+      symbol: '[太阳]',
+      name: 'taiyang',
+      title: '太阳'
+  },
+  {
+      symbol: '[拥抱]',
+      name: 'yongbao',
+      title: '拥抱'
+  },
+  {
+      symbol: '[强]',
+      name: 'qiang',
+      title: '强'
+  },
+  {
+      symbol: '[弱]',
+      name: 'ruo',
+      title: '弱'
+  },
+  {
+      symbol: '[握手]',
+      name: 'woshou',
+      title: '握手'
+  },
+  {
+      symbol: '[胜利]',
+      name: 'shengli',
+      title: '胜利'
+  },
+  {
+      symbol: '[抱拳]',
+      name: 'baoquan',
+      title: '抱拳'
+  },
+  {
+      symbol: '[勾引]',
+      name: 'gouyin',
+      title: '勾引'
+  },
+  {
+      symbol: '[拳头]',
+      name: 'quantou',
+      title: '拳头'
+  },
+  {
+      symbol: '[OK]',
+      name: 'ok',
+      title: 'ok'
+  },
+  {
+      symbol: '[篮球]',
+      name: 'lanqiu',
+      title: '篮球'
+  },
+  {
+      symbol: '[乒乓]',
+      name: 'pingpang',
+      title: '乒乓'
+  },
+  {
+      symbol: '[饭]',
+      name: 'fan',
+      title: '饭'
+  },
+  {
+      symbol: '[闪电]',
+      name: 'shandian',
+      title: '闪电'
+  },
+  {
+      symbol: '[刀]',
+      name: 'dao',
+      title: '刀'
+  },
+  {
+      symbol: '[足球]',
+      name: 'zuqiu',
+      title: '足球'
+  },
+  {
+      symbol: '[瓢虫]',
+      name: 'piaochong',
+      title: '瓢虫'
+  },
+  {
+      symbol: '[礼物]',
+      name: 'liwu',
+      title: '礼物'
+  },
+  {
+      symbol: '[差劲]',
+      name: 'chajin',
+      title: '差劲'
+  },
+  {
+      symbol: '[爱你]',
+      name: 'aini',
+      title: '爱你'
+  },
+  {
+      symbol: '[NO]',
+      name: 'no',
+      title: 'no'
+  }
+];
+
+export default qqfaceList;
